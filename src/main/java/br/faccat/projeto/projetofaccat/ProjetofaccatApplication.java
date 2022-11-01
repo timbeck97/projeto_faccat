f440394e2f85c6878ebad6fee7f2d908e5ae9958
Tim Beck <timbeck1997@hotmail.com>
2022-11-01 20:44:10 -0300

    commit inicial

---
+++ src/main/java/br/faccat/projeto/projetofaccat/ProjetofaccatApplication.java
@@ -0,0 +1,13 @@
+package br.faccat.projeto.projetofaccat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofaccatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofaccatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/faccat/projeto/projetofaccat/ProjetofaccatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.faccat.projeto.projetofaccat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofaccatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
