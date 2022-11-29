0dcdea1edcde8b807a6c745921e2328ceaffa08b
tim <tim@infisc.com.br>
2022-11-28 23:34:47 -0300

    swagger error controller ignore

--- src/main/java/br/faccat/projeto/projetofaccat/configuration/SwaggerConfig.java
+++ src/main/java/br/faccat/projeto/projetofaccat/configuration/SwaggerConfig.java
@@ -7,6 +7,7 @@ package br.faccat.projeto.projetofaccat.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -24,7 +25,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
           .select()
-          .apis(RequestHandlerSelectors.any())
+          .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
           .paths(PathSelectors.any())
           .build();
     }
